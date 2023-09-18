b60ed9fb24aaa39fdab6b80a227e0183c2c13f65
Paddy <paddy.goode2@mail.dcu.ie>
2023-09-18 22:30:19 +0100

    Update visualisingpreviousdata.py
    Added a red trendline.

--- visualisingpreviousdata.py
+++ visualisingpreviousdata.py
@@ -2,11 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.dates as mdates
+import numpy as np
 
 # Set a seaborn style for better aesthetics
 sns.set(style="whitegrid")
 
-# Load the dataset
+# Load the dataset 
 file_path = 'HPM09.20230918T210942.csv'
 df = pd.read_csv(file_path)
 
@@ -19,8 +20,14 @@ df_clean = df_clean.sort_values(by='Month')
 
 # Plotting
 plt.figure(figsize=(16, 6))
-sns.lineplot(x='Month', y='VALUE', data=df_clean, color='royalblue')
-plt.title('Monthly Percentage Change in Residential Property Prices in Dublin', fontsize=16)
+sns.lineplot(x='Month', y='VALUE', data=df_clean, color='royalblue', label='Monthly Change')
+
+# Add a trendline using numpy's polyfit
+z = np.polyfit(range(len(df_clean['Month'])), df_clean['VALUE'], 1)
+p = np.poly1d(z)
+plt.plot(df_clean['Month'], p(range(len(df_clean['Month']))), 'r--', label='Trendline')
+
+plt.title('Monthly Percentage Change in Residential Property Prices in Dublin with Trendline', fontsize=16)
 plt.xlabel('Month', fontsize=14)
 plt.ylabel('Percentage Change (%)', fontsize=14)
 
@@ -29,13 +36,9 @@ plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
 plt.gca().xaxis.set_major_locator(mdates.YearLocator())
 plt.gcf().autofmt_xdate()
 
-# Highlight important points or ranges (if any)
-# Example: Highlighting a year range
-# plt.axvspan('2020-01', '2021-01', color='grey', alpha=0.2, label='Year Range of Interest')
-
 # Add grid and legend
 plt.grid(True)
-# plt.legend()
+plt.legend()
 
 # Show plot
 plt.show()
