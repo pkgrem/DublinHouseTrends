e7537a64fc8ffb1c24362924c8d03c76fe4b3673
Paddy <paddy.goode2@mail.dcu.ie>
2023-09-18 22:27:28 +0100

    Update visualisingpreviousdata.py

--- visualisingpreviousdata.py
+++ visualisingpreviousdata.py
@@ -1,36 +1,41 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.dates as mdates
 
-# Load the dataset (Replace 'Your_File_Path_Here.csv' with the actual path to your CSV file)
+# Set a seaborn style for better aesthetics
+sns.set(style="whitegrid")
+
+# Load the dataset
 file_path = 'HPM09.20230918T210942.csv'
 df = pd.read_csv(file_path)
-
-# Display the first few rows of the dataset
-print(df.head())
-
-# Check for missing values
-missing_values = df.isnull().sum()
-print("Missing Values:", missing_values)
 
 # Remove rows with missing values
 df_clean = df.dropna()
 
-# Check the shape of the cleaned dataset
-print("Shape of cleaned dataset:", df_clean.shape)
-
-# Convert the 'Month' column to a datetime format
+# Convert 'Month' to datetime and sort
 df_clean['Month'] = pd.to_datetime(df_clean['Month'].str.strip(), format='%Y %B')
-
-# Sort the dataset by 'Month'
 df_clean = df_clean.sort_values(by='Month')
 
-# Exploratory Data Analysis (EDA)
-# Plotting the time series of monthly percentage change in house prices
+# Plotting
 plt.figure(figsize=(16, 6))
-sns.lineplot(x='Month', y='VALUE', data=df_clean)
-plt.title('Monthly Percentage Change in Residential Property Prices in Dublin')
-plt.xlabel('Month')
-plt.ylabel('Percentage Change (%)')
+sns.lineplot(x='Month', y='VALUE', data=df_clean, color='royalblue')
+plt.title('Monthly Percentage Change in Residential Property Prices in Dublin', fontsize=16)
+plt.xlabel('Month', fontsize=14)
+plt.ylabel('Percentage Change (%)', fontsize=14)
+
+# Improve x-axis formatting and ticks
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+plt.gca().xaxis.set_major_locator(mdates.YearLocator())
+plt.gcf().autofmt_xdate()
+
+# Highlight important points or ranges (if any)
+# Example: Highlighting a year range
+# plt.axvspan('2020-01', '2021-01', color='grey', alpha=0.2, label='Year Range of Interest')
+
+# Add grid and legend
 plt.grid(True)
+# plt.legend()
+
+# Show plot
 plt.show()
